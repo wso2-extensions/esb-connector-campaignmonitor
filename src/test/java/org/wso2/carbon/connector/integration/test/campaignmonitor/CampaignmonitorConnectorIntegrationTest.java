675661c3bf21a5b127c6833e7fbe93bb196b1c32
keetz <heykeetz@gmail.com>
2016-07-13 16:55:38 +0530

    After the release

--- src/test/java/org/wso2/carbon/connector/integration/test/campaignmonitor/CampaignmonitorConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/campaignmonitor/CampaignmonitorConnectorIntegrationTest.java
@@ -42,7 +42,7 @@ public class CampaignmonitorConnectorIntegrationTest extends ConnectorIntegratio
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("campaignmonitor-connector-1.0.1");
+        init("campaignmonitor-connector-1.0.2-SNAPSHOT");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
